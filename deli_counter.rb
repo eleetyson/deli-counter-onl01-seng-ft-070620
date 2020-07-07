e6aa9cfb003f1af15f2191de1c99526a9a70d424
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-07 18:40:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def line(arr)
+  str = "The line is currently"
+  if arr.empty?
+    puts str << " empty."
+  else
+    str += ": "
+    arr.each_with_index do |element, index|
+      if index == arr.length - 1
+        str << "#{index + 1}. #{element}"
+      else
+        str << "#{index + 1}. #{element} "
+      end
+    end
+    puts str
+  end
+end
+
+def take_a_number(arr, str)
+  arr << "#{str}"
+  puts "Welcome, #{str}. You are number #{arr.length} in line."
+end
+
+def now_serving(arr)
+  if arr.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{arr.first}."
+    arr.shift
+  end
+end
